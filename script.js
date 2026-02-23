dd82691351a00f88bb51a4d8d0d4b9c506c0a851
portalbenef <spyderhuracan8@gmail.com>
2026-02-23 11:46:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
    1. CONFIGURAÇÃO GERAL
    ========================================= */
 // ✅ Apenas números (sem +, sem espaços)
-const whatsappNumber = "552231900476";
+const whatsappNumber = "5511985509680";
 
 // Mensagem padrão
 const defaultMessage = "Olá, gostaria de iniciar meu atendimento.";
@@ -421,4 +421,5 @@ window.handleLogin = handleLogin;
 window.scrollToTop = scrollToTop;
 window.changeSlide = changeSlide;
 window.acceptCookies = acceptCookies;
+
 
